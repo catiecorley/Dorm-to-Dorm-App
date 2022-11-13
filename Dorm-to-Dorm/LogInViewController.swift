f058121cbf2951f3e951e0fd1f64b98a7f28deb2
jdulai2020 <jacobdulai@gmail.com>
2022-11-13 17:28:38 -0600

    Added firebase email sign in

--- Dorm-to-Dorm/LogInViewController.swift
+++ Dorm-to-Dorm/LogInViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LogInViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +20,31 @@ class LogInViewController: UIViewController {
     }
     
 
+    @IBAction func loginButtonClicked(_ sender: Any) {
+//        if usernameField.text != nil && passwordField.text != nil {
+//            Auth.auth().createUser(withEmail: usernameField.text!, password: passwordField.text!) { authResult, error in
+//                guard let user = authResult?.user, error == nil else {
+//                    print(error?.localizedDescription)
+//                    return
+//                }
+//
+//                let homeVC = self.storyboard?.instantiateViewController(withIdentifier: "homeVC") as! HomeViewController
+//                self.navigationController?.setViewControllers([homeVC], animated: false)
+//            }
+//        }
+        
+        if usernameField.text != nil && passwordField.text != nil {
+            Auth.auth().signIn(withEmail: usernameField.text!, password: passwordField.text!) {authResult, error in
+                guard let user = authResult?.user, error == nil else {
+                    print(error?.localizedDescription)
+                    return
+                }
+                
+                print("Email: " + user.email!)
+                // navigate to home screen
+                }
+            }
+    }
     /*
     // MARK: - Navigation
 
